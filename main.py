a84848b6fd5cbe0ec85e66bb151344b9c2a01c3b
BECHOUAL Ilyes <Ilyes.Bechoual@bordeaux-inp.fr>
2023-07-07 16:45:13 +0200

    fixing the main file

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from tkinter import *
+import ttkbootstrap as ttk
+
 
 #Création de la fenêtre d'accueil
-master = Tk()
+master = ttk.Window(themename= 'darkly')
 
 #Personnaliser la fenêtre
 master.title('Main page')
 master.minsize(720,480)
 #master.iconbitmap("images/logo_icon.ico")
-master.config(background='#41B77F')
+
 
 #Définition des fonctions qui amènent aux différentes pages
 def open_discussion():
@@ -31,30 +33,31 @@ def open_presentation():
         exec(code)
 
 #Création du header, donc de la frame
-frame=Frame(master, bg='#41B77F')
+frame=ttk.Frame(master)
 
-label_title = Label(frame, text="Template Header", font=("Helvetica", 40), bg='#41B77F', fg='black')
+label_title = ttk.Label(frame, text="Template Header", font="Helvetica 25")
 label_title.pack(pady=10)
 #Ajout de texte (penser à mettre le même fond que la fenêtre pour ne pas voir le rectangle)
 #Catégories Discussion, Statistiques, Réglages, Présentation
 
+
 #Ajout des boutons amenant aux différentes pages
-discussion_button=Button(frame, text="Discussion", font=("Helvetica", 25), bg='#41B77F', fg='black', command=open_discussion)
+discussion_button= ttk.Button(frame, text="Discussion", command=open_discussion)
 discussion_button.pack(side='left', pady=30)
 
-statistic_button=Button(frame, text="Stats", font=("Helvetica", 25), bg='#41B77F', fg='black', command=open_stats)
+statistic_button=ttk.Button(frame, text="Stats", command=open_stats)
 statistic_button.pack(side='left', pady=30)
 
-settings_button=Button(frame, text="Settings", font=("Helvetica", 25), bg='#41B77F', fg='black', command=open_settings)
+settings_button=ttk.Button(frame, text="Settings", command=open_settings)
 settings_button.pack(side='left', pady=30)
 
-presentation_button=Button(frame, text="Presentation", font=("Helvetica", 25), bg='#41B77F', fg='black', command=open_presentation)
+presentation_button=ttk.Button(frame, text="Presentation", command=open_presentation)
 presentation_button.pack(side='left', pady=30)
 frame.pack(side=TOP)
 
 
 
-label_subtitle = Label(master, text="Template header to help the user figure out how the interface is looking", font=("Helvetica", 20), bg='#41B77F', fg='black')
+label_subtitle = ttk.Label(master, text="Template header to help the user figure out how the interface is looking", font="Helvetica 12")
 label_subtitle.pack(side=TOP, pady=50)
 
 #Afficher la fenêtre
