fa1c6e38f482cbba1136208cdfdfeeb8f4af9c72
arthu <arthur.rubio@etu.mines-ales.fr>
2023-07-07 14:03:00 +0200

    add of the first window

--- interface.py
+++ interface.py
@@ -1,1 +1,12 @@
+from tkinter import*
 
+#Création de la fenêtre d'accueil
+master = Tk()
+
+#Personnaliser la fenêtre
+master.title('Accueil')
+master.state('zoomed')
+master.minsize(720,480)
+
+#Afficher la fenêtre
+master.mainloop()
