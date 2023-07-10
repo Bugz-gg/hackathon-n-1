edb9ee2fa2a4563405f35fed7e419d1117311251
LePetitMage <gong-jian.shao2003@laposte.net>
2023-07-10 17:16:46 +0200

    Reduced the risk of overfitting.

--- chat.py
+++ chat.py
@@ -75,18 +75,6 @@ def get_response(temperature : float = 0.9, chat_input : str = "I want you to re
                     output_text="Wonderful! It seems like you had a fun day at the park with your friends. Just a small adjustment: instead of saying \'Yesterday, I go to the park,\' it should be \'Yesterday, I went to the park.\' Remember to use the past tense when talking about actions in the past. Could you share more about your experience in the park?",
                 ),
                 InputOutputTextPair(
-                    input_text="I want to learning how to cook.",
-                    output_text="That\'s fantastic! It\'s great that you want to learn how to cook. Just a small correction: instead of saying \'I want to learning,\' it should be \'I want to learn.\' Keep up the enthusiasm! Can you tell us what specific dishes or cuisines you would like to learn?",
-                ),
-                InputOutputTextPair(
-                    input_text="My favorite movie is \'Titanic.\' It\'s very excited!",
-                    output_text="That\'s wonderful! \'Titanic\' is a classic movie. Just a small adjustment: instead of saying \'It\'s very excited,\' it should be \'It\'s very exciting.\' Keep up the good work! Could you share what you find most captivating about the movie?",
-                ),
-                InputOutputTextPair(
-                    input_text="I often go shopping in weekends with my sister.",
-                    output_text="That sounds like a fun activity! Just a small correction: instead of saying \'I often go shopping in weekends,\' it should be \'I often go shopping on weekends.\' Keep it up! Can you tell us about some of your favorite places to go shopping?"
-                ),
-                InputOutputTextPair(
                     input_text="I had an great day today !",
                     output_text="That\'s great to hear! I\'m glad you had a good day. Just a small correction: instead of saying \'I had an great day today,\' it should be \'I had a great day today.\' Keep up the good work!",
                 ),
@@ -103,38 +91,13 @@ def get_response(temperature : float = 0.9, chat_input : str = "I want you to re
                     output_text="Congratulations! Your sentence is flawless, and I\'m thrilled to hear about your progress in English. It\'s wonderful that you\'re feeling more confident. Can you share some strategies or resources that have helped you improve your language skills?",
                 ),
                 InputOutputTextPair(
-                    input_text="I enjoy to swimming in the beach during summertime and eat ice creams.",
-                    output_text="That\'s great! I love swimming in the beach during summertime too. Just a small adjustment: instead of saying \'I enjoy to swimming in the beach,\' it should be \'I enjoy swimming in the beach.\' Keep up the good work!",
-                ),
-
-                InputOutputTextPair(
                     input_text = "Hi, I'm excited to learn English. Where should I start?",
                     output_text = "Hello! That's great to hear. To begin your English language journey, we can start with some foundational topics like greetings, introductions, and basic vocabulary. Would you like to learn some common greetings?",
                 ),
-                    
-                InputOutputTextPair(
-                    input_text = "I find English grammar quite challenging. Can you help me with it?",
-                    output_text = "Of course! English grammar can be tricky, but with practice, it becomes easier. Is there a specific grammar topic you'd like to focus on, such as verb tenses, sentence structure, or parts of speech?",
-                ),
-                    
-                InputOutputTextPair(
-                    input_text = "I want to improve my English speaking skills. How can I practice?",
-                    output_text = "That's a great goal! To improve your speaking skills, it's important to practice regularly. One effective way is to engage in conversations. Would you like to have a conversation with me? We can discuss a topic of your choice.",
-                ),
-
                 InputOutputTextPair(
                     input_text = "I feel nervous when speaking English in public. Any tips to overcome this?",
                     output_text = "It's natural to feel nervous, but with practice and confidence-building techniques, you can overcome it. One helpful tip is to start by practicing in a comfortable environment, like with friends or in a language exchange group. Have you tried any of these methods before?",
                 ),
-
-                InputOutputTextPair(
-                    input_text = "I had an great day today!",
-                    output_text = "That's great to hear! I'm glad you had a good day. Just a small correction: instead of saying 'I had an great day today,' it should be 'I had a great day today.' Keep up the good work!",
-                ),
-                InputOutputTextPair(
-                    input_text="I often goes to the library to read books and improve my English.",
-                    output_text="That's a great habit! Just a small correction: instead of saying 'I often goes,' it should be 'I often go.' Going to the library and reading books is an excellent way to enhance your English skills. What kind of books do you enjoy reading?",
-                    ),
                 InputOutputTextPair(
                     input_text="I'm struggling with understanding English idioms. Can you help me with that?",
                     output_text="Of course! English idioms can be challenging, but with practice, you can become familiar with them. Is there a specific idiom you'd like to learn about, or do you need general guidance on how to understand and use idioms in conversations?",
@@ -144,44 +107,12 @@ def get_response(temperature : float = 0.9, chat_input : str = "I want you to re
                     output_text="That's a great goal! There are several ways to expand your vocabulary. One effective method is to read extensively in English. Are there any specific topics or genres you're interested in? Additionally, using vocabulary-building apps or flashcards can also be helpful. Have you tried any of these methods before?",
                     ),
                 InputOutputTextPair(
-                    input_text="I find it difficult to pronounce certain English sounds. Any tips to improve my pronunciation?",
-                    output_text="Pronunciation can be challenging, but with practice, you can improve. One tip is to listen to native English speakers and mimic their pronunciation. Another helpful approach is to focus on individual sounds and practice them using tongue twisters. Is there any specific sound or phoneme that you find particularly challenging?",
-                    ),
-                InputOutputTextPair(
-                    input_text="I struggle with understanding spoken English. How can I improve my listening skills?",
-                    output_text="Improving listening skills is essential for understanding spoken English. One effective method is to listen to English podcasts, watch movies or TV shows in English, and engage in conversations with native English speakers. Additionally, practicing active listening techniques, such as summarizing what you've heard, can also be beneficial. Have you tried any of these strategies?",
-                    ),
-                InputOutputTextPair(
-                    input_text="I'm interested in improving my English writing skills. Do you have any tips or resources?",
-                    output_text="Certainly! Improving writing skills takes practice. One tip is to start by writing regularly, even if it's just a journal entry or short paragraph. You can also seek feedback from native English speakers or consider taking writing courses online. Are there any specific writing areas or genres you'd like to focus on?",
-                    ),
-                InputOutputTextPair(
-                    input_text="English grammar rules confuse me. How can I better understand and apply them?",
-                    output_text="Grammar can be complex, but with patience and practice, you can improve your understanding. One approach is to study grammar rules systematically and then practice applying them through exercises or writing activities. Are there any specific grammar topics or rules you find particularly challenging?",
-                    ),
-                InputOutputTextPair(
-                    input_text="I want to build more confidence in speaking English. How can I overcome my shyness?",
-                    output_text="Building confidence in speaking English is a gradual process. One helpful strategy is to practice speaking with friends or language exchange partners who are also learning English. Another approach is to join conversation clubs or participate in language immersion programs. Have you tried any of these methods before?",
-                    ),
-                InputOutputTextPair(
-                    input_text="I feel overwhelmed by the amount of English vocabulary. How can I remember words more effectively?",
-                    output_text="Vocabulary retention can be challenging, but there are techniques that can help. One method is to create flashcards with new words and review them regularly. Another approach is to use vocabulary in context by incorporating new words into sentences or conversations. Are there any specific vocabulary topics or areas you'd like to focus on?",
-                    ),
-                InputOutputTextPair(
-                    input_text="I'm preparing for an English exam. Do you have any study tips?",
-                    output_text="Absolutely! When preparing for an English exam, it's important to practice all language skills: reading, writing, listening, and speaking. Make sure to review exam format and requirements, and dedicate time for focused practice in each area. Additionally, taking practice tests and seeking feedback can help you identify areas that need improvement. Which specific exam are you preparing for?",
-                    ),
-                InputOutputTextPair(
-                    input_text="I want to sound more natural in English. How can I improve my fluency?",
-                    output_text="Improving fluency requires consistent practice. One approach is to engage in conversations with native English speakers or participate in language exchange programs. Additionally, listening to English audio materials and mimicking natural speech patterns can help you develop a more natural flow. Have you tried practicing with native speakers before?",
-                    ),
-                InputOutputTextPair(
                         input_text="What does the word 'eclectic' mean?",
                         output_text="'Eclectic' refers to the selection or combination of elements from various sources or styles. It denotes a diverse and varied approach. Synonyms for 'eclectic' include diverse, varied, and multifaceted. Do you have any other words you'd like to know the meaning of?",
                         ),
                 InputOutputTextPair(
-                        input_text="How do you say 'delicious' in English?",
-                        output_text="The word for 'delicious' in English is 'delicious.' It describes something that tastes exceptionally good or pleasing to the senses. Do you have any other words or phrases you'd like to learn?",
+                        input_text="How do you say the french word 'delicieux' in English?",
+                        output_text="The french word 'delicieux' in English is 'delicious.' It describes something that tastes exceptionally good or pleasing to the senses. Do you have any other words or phrases you'd like to learn?",
                         ),
                 InputOutputTextPair(
                         input_text="What is the difference between 'affect' and 'effect'?",
@@ -196,18 +127,6 @@ def get_response(temperature : float = 0.9, chat_input : str = "I want you to re
                         output_text="English idioms are phrases that have a figurative meaning that differs from their literal interpretation. Here are a few examples of common idioms: 'break a leg' (meaning good luck), 'the ball is in your court' (meaning it's your turn to take action), and 'barking up the wrong tree' (meaning pursuing the wrong course of action). Idioms add color and richness to the English language. Let me know if you want to learn more idioms!",
                         ),
                 InputOutputTextPair(
-                        input_text="How do you form a question in English?",
-                        output_text="In English, questions are often formed by inverting the subject and the auxiliary verb. For example, instead of saying 'You are going,' the question form would be 'Are you going?' Additionally, question words like 'who,' 'what,' 'where,' 'when,' 'why,' and 'how' can be used to initiate questions. Would you like more examples or specific guidance on forming questions?",
-                        ),
-                InputOutputTextPair(
-                        input_text="What are the main tenses in English?",
-                        output_text="English has several tenses, but some of the main ones include present simple, past simple, and future simple. The present simple tense is used to describe habitual actions or general truths, the past simple tense is used to talk about completed actions in the past, and the future simple tense is used to express future actions. There are also progressive and perfect tenses for each time frame. Let me know if you want more information about specific tenses!",
-                        ),
-                InputOutputTextPair(
-                        input_text="How can I improve my English pronunciation?",
-                        output_text="Improving pronunciation takes practice. Some tips include listening to native English speakers, paying attention to word stress and intonation, and practicing individual sounds or phonemes. Mimicking native speakers and recording your own voice can also help you identify areas for improvement. Would you like more specific exercises or resources to improve your pronunciation?",
-                        ),
-                InputOutputTextPair(
                         input_text="What are some common abbreviations used in English?",
                         output_text="English has many common abbreviations, such as 'etc.' (short for 'et cetera'), 'e.g.' (short for 'exempli gratia,' meaning 'for example'), and 'i.e.' (short for 'id est,' meaning 'that is'). Abbreviations are often used to provide shortcuts or convey specific meanings. Let me know if you have any more questions about abbreviations or any other topic!",
                         ),
@@ -215,10 +134,6 @@ def get_response(temperature : float = 0.9, chat_input : str = "I want you to re
                         input_text="How do you use the word 'although' in a sentence?",
                         output_text="The word 'although' is used to introduce a subordinate clause that expresses a contrast or contradiction to the main clause. For example, 'Although it was raining, we still went for a walk.' It shows that there is a counterpoint to the main idea. 'Although' can be replaced with other similar words like 'despite' or 'even though.' If you have any more questions about sentence structure or usage, feel free to ask!",
                         ),
-                InputOutputTextPair(
-                        input_text= "Use the term perchence in a sentence",
-                        output_text= "Certainly! Here's an example sentence using the term \"perchance\": \"Perchance, I will stumble upon a hidden treasure during my adventurous journey. \"Remember, \"perchance\" is an old-fashioned term that means \"perhaps\" or \"maybe.\" It adds a touch of curiosity and possibility to the sentence. If you have any more questions or need assistance with anything else, feel free to ask!"
-                    ),
                     
             ],
         )
@@ -226,7 +141,6 @@ def get_response(temperature : float = 0.9, chat_input : str = "I want you to re
     response = chat.send_message(
         chat_input, **parameters
     )
-    # print(f"Response from Model: {response.text}\n")
 
     return response
 
