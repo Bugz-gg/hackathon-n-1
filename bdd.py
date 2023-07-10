ffd0ab20d3536503b2a2976c96b7ea5d872cebfd
LePetitMage <gong-jian.shao2003@laposte.net>
2023-07-10 12:53:45 +0200

    Ajout création table si elle n'existe pas.

--- bdd.py
+++ bdd.py
@@ -3,22 +3,24 @@ from datetime import *
 
 
 def create_table():
-    # Création de la table (elle est maintenant déjà créée)
+    bdd = sql.connect('chat.bd')
+    c = bdd.cursor()
     c.execute("""CREATE TABLE discussions(id_conversation INTEGER,
     id_utilisateur TEXT,
     id_message_conversation INTEGER,
     text_message TEXT,
     timestamp_message TEXT
     )""")
+    bdd.commit()
+    bdd.close()
 
 if __name__=="__main__":
+    create_table()
     # Création de la BDD
     bdd = sql.connect('chat.bd')
 
     # Création du curseur qui va permettre de faire des requetes SQL
     c = bdd.cursor()
-    create_table()
-
 
     # Instructions SQL pour insérer des données dans la BDD (j'ai inséré le premier message)
     # c.execute("INSERT INTO discussions VALUES (1,1,1, 'premier message', '2020-01-01 00:00')")
--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import ttkbootstrap as ttk
 import sqlite3 as sql
 from datetime import *
 from chat import *
-import sys
+from bdd import create_table
 
-#Définition des variables utiles au chat avec le bot afin d'éviter des erreurs
-global text_sent_messages 
+try:
+    create_table()
+except:
+    pass
+
+# Définition des variables utiles au chat avec le bot afin d'éviter des erreurs
+global text_sent_messages
 reconnect = True
-#Classe principale de la page
+
+
+# Classe principale de la page
 class Page:
     def __init__(self, master, title, content):
         self.master = master
@@ -16,27 +23,29 @@ class Page:
         self.create_widgets(title, content)
 
     def create_widgets(self, title, content):
-        #Personnaliser la fenêtre
+        # Personnaliser la fenêtre
         self.master.title(title)
         self.master.attributes("-fullscreen", True)
-        #self.master.minsize(720,480)
+        # self.master.minsize(720,480)
 
-        #Création du header, donc de la frame
-        button_frame=Frame(self.master)
-        button_frame.pack(side=TOP, pady = 30)
+        # Création du header, donc de la frame
+        button_frame = Frame(self.master)
+        button_frame.pack(side=TOP, pady=30)
 
         for button_title in ["Discussion", "Stats", "Settings", "Presentation"]:
             if button_title == title:
-                #Permet de désactiver le bouton de la page lorsque l'on est déjà dessus
-                button = ttk.Button(button_frame, text=button_title, command=lambda x=button_title:open_page(x), state="disabled")
+                # Permet de désactiver le bouton de la page lorsque l'on est déjà dessus
+                button = ttk.Button(button_frame, text=button_title, command=lambda x=button_title: open_page(x),
+                                    state="disabled")
             else:
-                button = ttk.Button(button_frame, text=button_title, command=lambda x=button_title:open_page(x), state="enabled")
+                button = ttk.Button(button_frame, text=button_title, command=lambda x=button_title: open_page(x),
+                                    state="enabled")
             button.pack(side=LEFT)
-        button = ttk.Button(button_frame, text="Exit", command=lambda :self.master.destroy(), state="enable")
+        button = ttk.Button(button_frame, text="Exit", command=lambda: self.master.destroy(), state="enable")
         button.pack(side=LEFT)
 
-        frame=Frame(self.master)
-        frame.pack(side=TOP, pady= 20)
+        frame = Frame(self.master)
+        frame.pack(side=TOP, pady=20)
 
         # Set the flag variables to track initialization
         label_title_initialized = False
@@ -47,21 +56,25 @@ class Page:
             label_title.pack()
 
         content()
-        
 
-#Ce qui s'affiche lorsqu'on lance l'application pour la première fois
+
+# Ce qui s'affiche lorsqu'on lance l'application pour la première fois
 def main_content():
-    label_subtitle = Label(root.master, text="Click on Discussion to start learning english with an amazing english teacher\n  Or click on presentation if you want to learn more about the fluencia project.", font=("Helvetica", 12))
+    label_subtitle = Label(root.master,
+                           text="Click on Discussion to start learning english with an amazing english teacher\n  Or click on presentation if you want to learn more about the fluencia project.",
+                           font=("Helvetica", 12))
     label_subtitle.pack(side=TOP, pady=50)
     pass
 
-#Fonction qui gère la BDD de la page de discussion
+
+# Fonction qui gère la BDD de la page de discussion
 def discussion_content():
     def save_data():
         global reconnect
         entry_value = entry_string.get()
 
-        formatted_timestamp = datetime.strptime(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S').strftime('%m-%d %H:%M')
+        formatted_timestamp = datetime.strptime(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                                '%Y-%m-%d %H:%M:%S').strftime('%m-%d %H:%M')
         formatted_message = f"[{formatted_timestamp}] You -- {entry_value}"
         text_sent_messages.configure(state='normal')
         text_sent_messages.insert(END, formatted_message + "\n")
@@ -84,9 +97,9 @@ def discussion_content():
                     else:
                         past += f"You replied : {message[0]}\n"
                 past += "Now the user says :"
-            #print(past)
+            # print(past)
             reconnect = False
-        output_value = science_tutoring(chat_input= past+entry_value).text + "\n"
+        output_value = science_tutoring(chat_input=past + entry_value).text + "\n"
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         bdd = sql.connect('chat.bd')
         c = bdd.cursor()
@@ -96,16 +109,19 @@ def discussion_content():
             current_id = result[0] + 1
         else:
             current_id = 1
-        c.execute("INSERT INTO discussions(id_conversation, id_utilisateur, id_message_conversation, text_message, timestamp_message) VALUES (?, ?, ?, ?, ?)",
-                  (1, "You", current_id, entry_value, timestamp))
-        c.execute("INSERT INTO discussions(id_conversation, id_utilisateur, id_message_conversation, text_message, timestamp_message) VALUES (?, ?, ?, ?, ?)",
-                  (1, "Bot", current_id + 1, output_value, timestamp))
+        c.execute(
+            "INSERT INTO discussions(id_conversation, id_utilisateur, id_message_conversation, text_message, timestamp_message) VALUES (?, ?, ?, ?, ?)",
+            (1, "You", current_id, entry_value, timestamp))
+        c.execute(
+            "INSERT INTO discussions(id_conversation, id_utilisateur, id_message_conversation, text_message, timestamp_message) VALUES (?, ?, ?, ?, ?)",
+            (1, "Bot", current_id + 1, output_value, timestamp))
         bdd.commit()
         bdd.close()
         display_messages(text_sent_messages)
         entry_string.set("")
         entry.config(textvariable=entry_string)
-    global l, i , tab
+
+    global l, i, tab
     l, i, tab = 0, 0, []
     new_lab = Label(root.master)
 
@@ -128,7 +144,8 @@ def discussion_content():
         text_sent_messages.tag_add("color", "{}.0".format(k[0]), "{}.{}".format(k[0], k[1]))
     pass
 
-#Fonction affichant les messages envoyés (id_utilisateur de l'usager =1)
+
+# Fonction affichant les messages envoyés (id_utilisateur de l'usager =1)
 def display_messages(text_widget):
     bdd = sql.connect('chat.bd')
     c = bdd.cursor()
@@ -149,21 +166,25 @@ def display_messages(text_widget):
             print(content, nom_utilisateur)
             global l, i
             l = len(content)
-            tab.append([i, l]) 
+            tab.append([i, l])
             print(l)
-        i+=1
+        i += 1
     text_widget.see(END)
     text_widget.configure(state='disabled', wrap=WORD)
 
-#Fonction définissant le contenu de la page stats
+
+# Fonction définissant le contenu de la page stats
 def stats_content():
     label_subtitle = Label(root.master, text="Coming soon.", font=("Helvetica", 20))
     label_subtitle.pack(side=TOP, pady=25)
 
-#Fonction définissant le contenu de la page de réglages
+
+# Fonction définissant le contenu de la page de réglages
 def settings_content():
     label_subtitle = Label(root.master, text="Coming soon.", font=("Helvetica", 20))
     label_subtitle.pack(side=TOP, pady=25)
+
+
 def presentation_content():
     def txt():
         return """
@@ -191,25 +212,26 @@ Thank you for your attention!
 
     label_subtitle = Label(root.master, text="Presentation time.", font=("Helvetica", 12))
     label_subtitle.pack(side=TOP, pady=25)
-    label_subsubtitle = Text(root.master,  wrap=WORD)
+    label_subsubtitle = Text(root.master, wrap=WORD)
     label_subsubtitle.insert(END, txt())
-    label_subsubtitle.pack( fill=Y)
+    label_subsubtitle.pack(fill=Y)
     label_subsubtitle.configure(state='disabled')
     pass
 
 
-get_content = {"Main": main_content, "Discussion": discussion_content, "Stats": stats_content, "Settings": settings_content, "Presentation": presentation_content}
+get_content = {"Main": main_content, "Discussion": discussion_content, "Stats": stats_content,
+               "Settings": settings_content, "Presentation": presentation_content}
 
-
-#Création de la fenêtre d'accueil
+# Création de la fenêtre d'accueil
 root = ttk.Window(themename='darkly')
 displayed_page = Page(root, "Welcome to Fluencia", main_content)
 
 
 def open_page(title):
     global displayed_page
-    displayed_page.master.destroy() # Fermer la fenêtre principale
+    displayed_page.master.destroy()  # Fermer la fenêtre principale
     displayed_page = Page(ttk.Window(themename='darkly'), title, get_content[title])
     displayed_page.master.mainloop()
+
 
 root.mainloop()
